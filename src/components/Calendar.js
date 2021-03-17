74a095254885aabcc7d089597a97acde20101042
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-17 15:47:51 -0600

    Update Calendar.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,13 +1,50 @@
 import React from "react";
-import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
+import {
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+} from "@syncfusion/ej2-react-schedule";
 
-export default function Calendar() {
-  return (
-    <div>
-      <ScheduleComponent>
-        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
-      </ScheduleComponent>
-      ;
-    </div>
-  );
+class Calendar extends React.Component {
+  onPopupOpen(args) {
+    console.log("hello");
+  }
+
+  //not work
+  onEditorClose() {
+    console.log("g'bye");
+  }
+
+  onActionBegin(ActionEventArgs) {
+    if (ActionEventArgs.changedRecords !== undefined) {
+      console.log(
+        ActionEventArgs.data[0].Subject,
+        ActionEventArgs.data[0].Id,
+        ActionEventArgs.data[0].IsAllDay,
+        ActionEventArgs.data[0].StartTime,
+        ActionEventArgs.data[0].EndTime
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ScheduleComponent
+          popupOpen={this.onPopupOpen.bind(this)}
+          editorClose={this.onEditorClose.bind(this)}
+          actionBegin={this.onActionBegin.bind(this)}
+        >
+          <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+        </ScheduleComponent>
+        ;
+      </div>
+    );
+  }
 }
+
+export default Calendar;
