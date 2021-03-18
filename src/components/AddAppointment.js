0c63dc5595bf8e2bdbf635c009f0c1dd033016f0
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-17 19:57:58 -0600

    Update AddAppointment.js

--- src/components/AddAppointment.js
+++ src/components/AddAppointment.js
@@ -4,11 +4,12 @@ import { projectFirestore } from "../firebase";
 export default function AddAppointment(appointment) {
   const collectionRef = projectFirestore.collection("appointment");
 
-  collectionRef.doc(appointment.Subject).set({
+  collectionRef.doc().set({
     id: appointment.Id.toString(),
     appointmentType: appointment.Subject,
     startTime: appointment.StartTime,
     endTime: appointment.EndTime,
+    isAllDay: appointment.IsAllDay,
   });
 
   return <div></div>;
