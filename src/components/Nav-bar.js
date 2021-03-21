0e2e753db041f5f0bddbc5c6553c5c776d51f5ce
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-21 16:39:17 -0600

    Update Nav-bar.js

--- src/components/Nav-bar.js
+++ src/components/Nav-bar.js
@@ -25,7 +25,7 @@ export default function NavBar({ currentUser }) {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">Calendar Dark Side</Navbar.Brand>
+        <Navbar.Brand href="/">CalendarDS</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
