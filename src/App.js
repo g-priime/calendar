329175da8e9c27ed70ba24afe0d001f432aa309c
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-20 21:03:04 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import './App.css';
+import PrivateRoute from "./components/PrivateRoute";
 import Calendar from './components/Calendar';
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import ForgotPassword from "./components/ForgotPassword";
 
 function App() {
   return (
     <div>
-      <Calendar />
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Calendar} />
+
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+          </Switch>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
