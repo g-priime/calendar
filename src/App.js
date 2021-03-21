6abefe3950a10134786b9254b2bef0500014645a
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-21 14:20:30 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import NavBar from "./components/NavBar";
 
 function App() {
   return (
