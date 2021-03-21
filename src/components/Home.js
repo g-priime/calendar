fe14699c323245ee2f491c70bcf5d9276d5248a2
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-21 14:20:15 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,16 @@
 import React from "react";
 import Calendar from "./Calendar";
-import { Button } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 import NavBar from "./Nav-bar";
 
 export default function Home() {
-  const history = useHistory();
-  const { logout } = useAuth();
 
   const { currentUser } = useAuth();
 
-  async function handleLogout() {
-    //closeMobileMenu();
-    //setError("");
-
-    try {
-      await logout();
-      history.push("/login");
-    } catch {
-      //setError("Failed to log out");
-    }
-  }
-
   return (
     <div>
-      <NavBar />
-      
+      <NavBar currentUser={currentUser} />   
       <Calendar currentUser={currentUser} />
     </div>
   );
