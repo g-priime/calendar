ad8509f4dab8d5aa95d36222e742d694b0c58192
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-21 16:34:19 -0600

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -39,32 +39,35 @@ export default function Login() {
         style={{ minHeight: "100vh" }}
       >
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Card.Body>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" ref={emailRef} required />
-              </Form.Group>
-              <Form.Group id="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} required />
-              </Form.Group>
-              <Button
-                disabled={loading}
-                className="w-100 button-forms"
-                type="submit"
-              >
-                Log In
-              </Button>
-            </Form>
-            <div className="w-100 text-center mt-3">
-              <Link to="/forgot-password">Forgot Password?</Link>
+          <Card border='primary' bg="dark" text="light">
+            <Card.Body>
+              <Card.Title>Log In to CalendarDS</Card.Title>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
+                <Button
+                  disabled={loading}
+                  className="w-100 button-forms"
+                  type="submit"
+                >
+                  Log In
+                </Button>
+              </Form>
+              <div className="w-100 text-center mt-3">
+                <Link to="/forgot-password">Forgot Password?</Link>
+              </div>
+            </Card.Body>
+            <div className="w-100 text-center mt-2">
+              Need an account? <Link to="/signup">Sign Up</Link>
             </div>
-          </Card.Body>
-          <div className="w-100 text-center mt-2">
-            Need an account? <Link to="/signup">Sign Up</Link>
-          </div>
+          </Card>
         </div>
       </Container>
     </div>
