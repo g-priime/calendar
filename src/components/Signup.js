1c0630b8ee8762a7c324fe74fb695a71801fe571
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-20 21:35:03 -0600

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert, Container } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
-import TitleLogInPage from "./TitleLogInPage";
+//import TitleLogInPage from "./TitleLogInPage";
 
 export default function Signup() {
   const emailRef = useRef();
@@ -17,7 +17,7 @@ export default function Signup() {
   //const [currentUser, setCurrentUser] = useState();
   //const { updateDisplayName } = useAuth();
 
-  const pageTitle = "Create Your Account";
+  //const pageTitle = "Create Your Account";
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -62,7 +62,9 @@ export default function Signup() {
 
   return (
     <div className="ImageGallery">
-      <TitleLogInPage pageTitle={pageTitle} />
+      {
+      //<TitleLogInPage pageTitle={pageTitle} />
+}
       <Container
         className="d-flex justify-content-center"
         style={{ minHeight: "100vh" }}
