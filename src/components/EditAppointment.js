617d1c496f8c48c1069d12fad7049aa4793bbcbc
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-21 19:31:51 -0600

    Update EditAppointment.js

--- src/components/EditAppointment.js
+++ src/components/EditAppointment.js
@@ -7,11 +7,13 @@ export default function EditAppointment(appointment) {
   collectionRef
     .doc(appointment.id)
     .update({
-        id: appointment.Id.toString(),
-        appointmentType: appointment.Subject,
-        startTime: appointment.StartTime,
-        endTime: appointment.EndTime,
-        isAllDay: appointment.IsAllDay,
+      id: appointment.Id.toString(),
+      appointmentType: appointment.Subject,
+      location: appointment.Location || "not specified",
+      startTime: appointment.StartTime,
+      endTime: appointment.EndTime,
+      isAllDay: appointment.IsAllDay,
+      description: appointment.Description || "not specified",
     })
     .then(() => {
       console.log("Document successfully updated!");
