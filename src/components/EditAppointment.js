2a271fb65134f566be049a399638fc50eb8f0719
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-21 22:32:28 -0600

    Update EditAppointment.js

--- src/components/EditAppointment.js
+++ src/components/EditAppointment.js
@@ -4,21 +4,35 @@ import { projectFirestore } from "../firebase";
 export default function EditAppointment(appointment) {
   const collectionRef = projectFirestore.collection("appointment");
 
-  collectionRef
-    .doc(appointment.id)
-    .update({
-      id: appointment.Id.toString(),
-      appointmentType: appointment.Subject,
-      location: appointment.Location || "not specified",
-      startTime: appointment.StartTime,
-      endTime: appointment.EndTime,
-      isAllDay: appointment.IsAllDay,
-      description: appointment.Description || "not specified",
-      recurrenceRule: appointment.RecurrenceRule || "FREQ=NEVER",
-    })
-    .then(() => {
-      console.log("Document successfully updated!");
-    });
+  if (
+    appointment.RecurrenceID !== undefined &&
+    appointment.RecurrenceID !== null
+  ) {
+    collectionRef
+      .doc(appointment.id)
+      .update({
+        recurrenceException: appointment.RecurrenceException || "none",
+      })
+      .then(() => {
+        console.log("Document successfully updated!");
+      });
+  } else {
+    collectionRef
+      .doc(appointment.id)
+      .update({
+        id: appointment.Id.toString(),
+        appointmentType: appointment.Subject,
+        location: appointment.Location || "not specified",
+        startTime: appointment.StartTime,
+        endTime: appointment.EndTime,
+        isAllDay: appointment.IsAllDay,
+        description: appointment.Description || "not specified",
+        recurrenceRule: appointment.RecurrenceRule || "FREQ=NEVER",
+      })
+      .then(() => {
+        console.log("Document successfully updated!");
+      });
+  }
 
   return <div></div>;
 }
