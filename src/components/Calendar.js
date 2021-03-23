677f413e9a2cd10978ca4e8b1e4f2ed5083d0bec
g-priime <57049481+g-priime@users.noreply.github.com>
2021-03-23 17:50:40 -0600

    Update Calendar.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -13,6 +13,9 @@ import { projectFirestore } from "../firebase";
 import EditAppointment from "./EditAppointment";
 import DeleteAppointment from "./DeleteAppointment";
 
+import { DateTimePickerComponent } from '@syncfusion/ej2-react-calendars';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
 class Calendar extends React.Component {
   constructor() {
     super(...arguments);
@@ -93,7 +96,7 @@ class Calendar extends React.Component {
         ActionEventArgs.data[0].Description,
         ActionEventArgs.data[0].RecurrenceID,
         ActionEventArgs.data[0].RecurrenceRule,
-        ActionEventArgs.data[0].RecurrenceException,
+        ActionEventArgs.data[0].RecurrenceException
       );
       AddAppointment(ActionEventArgs.data[0], this.props.currentUser);
       this.getInfo();
@@ -112,6 +115,87 @@ class Calendar extends React.Component {
     }
   }
 
+  editorTemplate(props) {
+    return props !== undefined ? (
+      <table
+        className="custom-event-editor"
+        style={{ width: "100%", cellpadding: "5" }}
+      >
+        <tbody>
+          <tr>
+            <td className="e-textlabel">Summary</td>
+            <td colSpan={4}>
+              <input
+                id="Summary"
+                className="e-field e-input"
+                type="text"
+                name="Subject"
+                style={{ width: "100%" }}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td className="e-textlabel">Status</td>
+            <td colSpan={4}>
+              <DropDownListComponent
+                id="EventType"
+                placeholder="Choose status"
+                data-name="EventType"
+                className="e-field"
+                style={{ width: "100%" }}
+                dataSource={["New", "Requested", "Confirmed"]}
+                value={props.EventType || null}
+              ></DropDownListComponent>
+            </td>
+          </tr>
+          <tr>
+            <td className="e-textlabel">From</td>
+            <td colSpan={4}>
+              <DateTimePickerComponent
+                format="dd/MM/yy hh:mm a"
+                id="StartTime"
+                data-name="StartTime"
+                value={new Date(props.startTime || props.StartTime)}
+                className="e-field"
+              ></DateTimePickerComponent>
+            </td>
+          </tr>
+          <tr>
+            <td className="e-textlabel">To</td>
+            <td colSpan={4}>
+              <DateTimePickerComponent
+                format="dd/MM/yy hh:mm a"
+                id="EndTime"
+                data-name="EndTime"
+                value={new Date(props.endTime || props.EndTime)}
+                className="e-field"
+              ></DateTimePickerComponent>
+            </td>
+          </tr>
+          <tr>
+            <td className="e-textlabel">Reason</td>
+            <td colSpan={4}>
+              <textarea
+                id="Description"
+                className="e-field e-input"
+                name="Description"
+                rows={3}
+                cols={50}
+                style={{
+                  width: "100%",
+                  height: "60px !important",
+                  resize: "vertical",
+                }}
+              ></textarea>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    ) : (
+      <div></div>
+    );
+  }
+
   render() {
     this.getAppointmentInfo();
 
@@ -124,6 +208,7 @@ class Calendar extends React.Component {
           eventSettings={{
             dataSource: this.state.data,
           }}
+          editorTemplate={this.editorTemplate.bind(this)}
         >
           <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
         </ScheduleComponent>
